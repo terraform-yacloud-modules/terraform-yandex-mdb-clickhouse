c0d9d3614a2c525bbc423b60a5cf3c4b14e1d28d
Anton Patsev <patsev.anton@gmail.com>
2024-07-29 11:58:35 +0600

    Добавил module network в пример

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,5 @@
+data "yandex_client_config" "client" {}
+
 module "iam_accounts" {
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-iam.git//modules/iam-account?ref=v1.0.0"
 
@@ -12,11 +14,30 @@ module "iam_accounts" {
 
 }
 
+module "network" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  blank_name = "vpc-nat-gateway"
+  labels = {
+    repo = "terraform-yacloud-modules/terraform-yandex-vpc"
+  }
+
+  azs = ["ru-central1-a"]
+
+  private_subnets = [["10.4.0.0/24"]]
+
+  create_vpc         = true
+  create_nat_gateway = true
+}
+
+
 module "clickhouse" {
   source = "../"
 
-  network_id = "xxx"
-  subnet_id  = "xxx"
+  network_id = module.network.vpc_id
+  subnet_id  = module.network.private_subnets_ids[0]
 
   cluster_name = "test-clickhouse-cluster"
 
@@ -25,4 +46,6 @@ module "clickhouse" {
   user_password = "your_password"
 
   service_account_id = module.iam_accounts.id
+
+  depends_on = [module.iam_accounts, module.network]
 }
