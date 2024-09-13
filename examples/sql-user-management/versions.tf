ac2f4f89dc8f193940b62054d5e47c16443aa268
Alexander <kvendingoldo@yandex.ru>
2024-09-13 17:06:42 +0200

    fix: provider version for example

--- examples/sql-user-management/versions.tf
+++ examples/sql-user-management/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.124.0"
+      version = "> 0.8"
     }
     tls = {
       source  = "hashicorp/tls"
