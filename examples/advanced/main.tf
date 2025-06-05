0362a78e9f0b424f47b5bb7b62e4f8f6047c8f1e
Anton Patsev <patsev.anton@gmail.com>
2025-06-05 12:41:47 +0600

    add advanced example

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -1,19 +1,5 @@
 data "yandex_client_config" "client" {}
 
-# module "iam_accounts" {
-#   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-iam.git//modules/iam-account?ref=v1.0.0"
-#
-#   name = "iam"
-#   folder_roles = [
-#     "admin",
-#   ]
-#   cloud_roles              = []
-#   enable_static_access_key = false
-#   enable_api_key           = false
-#   enable_account_key       = false
-#
-# }
-
 module "network" {
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
 
